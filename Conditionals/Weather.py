9fe44eb110e7d27d7ca7fab1c3ee48b0bfe26417
Gamedosa <gabriel20melo@gmail.com>
2025-11-02 09:00:02 -0300

    FEAT: Conditionals and random module.

---
+++ Conditionals/Weather.py
@@ -0,0 +1,15 @@
+temperature = 32
+
+if temperature > 80:
+    print("it's hot. Stay inside!!")
+elif temperature < 50:
+    print("It's too coold! Stay Inside!")
+else:
+    print("Enjoy the outdoors!")
+
+age = 21
+if age >= 21:
+    print('You can drink!')
+else:
+    print("You are underage!!!")
+
